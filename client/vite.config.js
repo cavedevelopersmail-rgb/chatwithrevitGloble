a1622d848fd14b3bcedfa00b92f019818d6f0987
pretam <pretamram0@gmail.com>
2025-12-11 18:40:27 +0530

    all done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // Import the React plugin
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    react(), // <--- Essential for your React code to work
+    tailwindcss(),
+  ],
 });
